5a471e98249b4b4d5cd82ef73237d5ba40e6cdb7
Bear Cabbage <xiaoxiongbaicai@outlook.com>
2018-08-17 09:35:15 +0800

    The Second Edit (Not Finish But Give Up)

---
+++ src/ main/ java/ top/ bearcabbage/ bedwars/BedVar.java
@@ -0,0 +1,24 @@
+package top.bearcabbage.bedwars;
+
+import cn.nukkit.level.Location;
+import cn.nukkit.plugin.PluginBase;
+
+
+/*
+This Class Created by Bear Cabbage on 2018/08/17.
+这个类由小熊白菜与2018/08/17创建。
+______                   _____       _     _
+| ___ \                 /  __ \     | |   | |
+| |_/ / ___  __ _ _ __  | /  \/ __ _| |__ | |__   __ _  __ _  ___
+| ___ \/ _ \/ _` | '__| | |    / _` | '_ \| '_ \ / _` |/ _` |/ _ \
+| |_/ /  __/ (_| | |    | \__/\ (_| | |_) | |_) | (_| | (_| |  __/
+\____/ \___|\__,_|_|     \____/\__,_|_.__/|_.__/ \__,_|\__, |\___|
+                                                        __/ |
+                                                       |___/
+*/
+public class BedVar extends PluginBase {
+    public Location[] goldblock = new Location[4];
+    goldblock[1]
+
+
+}
